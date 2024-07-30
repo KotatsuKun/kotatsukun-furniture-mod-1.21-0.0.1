63974586a2819b33571edd95a3b0f70f3ad3b0b1
Pedro paulo Munhoz massad <munhozpedropaulo@gmail.com>
2024-07-30 16:48:15 -0300

    Initial commit

---
+++ src/main/java/net/kotatsu/kun/furnt/mod/KotatsuKunFurnitureModClient.java
@@ -0,0 +1,11 @@
+package net.kotatsu.kun.furnt.mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KotatsuKunFurnitureModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
